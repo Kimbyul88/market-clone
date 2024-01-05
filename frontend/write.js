4630f4790950daced86d027ddebcc350f3001091
Kimbyul88 <byul88byul@gmail.com>
2024-01-05 15:50:25 +0900

    🪲버그 수정
    - 이미지 다시 띄웠어요
    - 가격 뒤에 원 붙였어요
    - 가격 3자리 마다 쉼표 붙였어요

--- frontend/write.js
+++ frontend/write.js
@@ -2,13 +2,13 @@ const form = document.getElementById("write-form");
 
 const handleSubmitForm = async (event) => {
   event.preventDefault();
+  const body = new FormData(form);
+  body.append("insertAt", new Date().getTime());
   try {
-    const formData = new FormData(form);
-
     const res = await fetch("/items", {
       //items는 데이터를 보내는 주소
       method: "POST",
-      body: new FormData(form),
+      body, // body:body와 같음!
     });
     const data = await res.json(); //리턴한 값!
 
